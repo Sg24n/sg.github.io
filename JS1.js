3162d2ea57ab1a417d8276be69c36c34b62555fe
Sg24n <tata.vaskhoma@gmail.com>
2021-10-13 18:52:47 +0300

    a

---
+++ JS1.js
@@ -0,0 +1,37 @@
+var a = Math.random(1);
+
+console.log(a);
+//Отображение результата в консоли
+if (a<=0.1){console.log(1)}
+else if  (a<=0.2){console.log(2)}
+else if  (a<=0.3){console.log(3)}
+else if  (a<=0.4){console.log(4)}
+else if  (a<=0.5){console.log(5)}
+else if  (a<=0.6){console.log(6)}
+else if  (a<=0.7){console.log(7)}
+else if  (a<=0.8){console.log(8)}
+else if  (a<=0.9){console.log(9)}
+else {console.log(10)}
+
+/*
+//Вывод сообщений
+if (a<=0.1){alert('Твая мама слочайна не ')}
+else if  (a<=0.2){alert('Тодi зведкi в ней такая')}
+else if  (a<=0.3){alert('asd')}
+else if  (a<=0.4){console.log(4)}
+else if  (a<=0.5){console.log(5)}
+else if  (a<=0.6){console.log(6)}
+else if  (a<=0.7){console.log(7)}
+else if  (a<=0.8){console.log(8)}
+else if  (a<=0.9){console.log(9)}
+else {console.log(10)}а
+*/
+
+var b = Math.random()
+let c = 'человек'
+if (b<=0.1){c = 'пекар'}
+else {с = 'asd'};
+
+alert('Твоя мама случайно не ' +c+' ?')
+
+
