aed3804c9ff87fcd7b83629d32ad1aaa06dde68c
Sg24n <tata.vaskhoma@gmail.com>
2021-10-14 11:30:42 +0300

    а

--- JS1.js
+++ JS1.js
@@ -6,19 +6,6 @@ function randomNumber() {
 
 
 console.log(randomNumber());
-//Отображение результата в консоли
-//if (randomNumber <= 0.1) { console.log(1) }
-//else if (randomNumber() <= 0.2) { console.log(2) }
-//else if (randomNumber() <= 0.3) { console.log(3) }
-//else if (randomNumber() <= 0.4) { console.log(4) }
-//else if (randomNumber() <= 0.5) { console.log(5) }
-//else if (randomNumber() <= 0.6) { console.log(6) }
-//else if (randomNumber() <= 0.7) { console.log(7) }
-//else if (randomNumber() <= 0.8) { console.log(8) }
-//else if (randomNumber() <= 0.9) { console.log(9) }
-//else { console.log(10) }
-
-
 
 var answer
 var answer2
