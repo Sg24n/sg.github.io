ee9a6153437982efe1513548fba89c8587162292
Sg24n <tata.vaskhoma@gmail.com>
2021-10-14 14:19:59 +0300

    a

--- JS1.js
+++ JS1.js
@@ -1,24 +1,29 @@
-function randomNumber() {
-    var randNumber = Math.random()
-    return randNumber
+// function randomNumber() {
+//     var randNumber = Math.random()
+//     return randNumber
+// }
+
+
+
+// console.log(randomNumber());
+
+// var answer
+// var answer2
+
+// if (randomNumber() <= 0.3) { answer = 'пекарь' }
+// else if (randomNumber() <= 0.4) { answer = 'плотник' }
+// else if (randomNumber()<=0.9){answer='мне наверное нужно научиться разметке...'}
+// else { answer = 'отец' };
+
+
+// if (randomNumber() <= 0.3) { answer2 = 'й молоток' }
+// else if (randomNumber() <= 0.4) { answer2 = 'й член' }
+// else { answer2 = 'е печенье' };
+
+// alert('Твоя мама случайно не ' + answer + ' ? Тогда откуда у неё тако' + answer2 + ' ?')
+
+var numbers
+
+if (numbers!==0) {
+    alert('О повезло повезло')
 }
-
-
-
-console.log(randomNumber());
-
-var answer
-var answer2
-
-if (randomNumber() <= 0.3) { answer = 'пекарь' }
-else if (randomNumber() <= 0.4) { answer = 'плотник' }
-else if (randomNumber()<=0.9){answer='мне наверное нужно научиться разметке...'}
-else { answer = 'отец' };
-
-
-if (randomNumber() <= 0.3) { answer2 = 'й молоток' }
-else if (randomNumber() <= 0.4) { answer2 = 'й член' }
-else { answer2 = 'е печенье' };
-
-alert('Твоя мама случайно не ' + answer + ' ? Тогда откуда у неё тако' + answer2 + ' ?')
-
